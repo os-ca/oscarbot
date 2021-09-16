a340651c5ab8633788b2055991d519f124312283
os-ca <88908154+os-ca@users.noreply.github.com>
2021-09-15 20:40:32 -0700

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -3,6 +3,6 @@ import os
 # Menu Interface
 if __name__ == '__main__':
     for f in os.listdir():
-        if f.startswith('main'):
+        if f.startswith('oscarbot'):
             os.system(f)
     
